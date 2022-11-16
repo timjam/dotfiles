73cb96ca69241b442c3d220993037e5d296a99de
Timo Mätäsaho <timo.matasaho@vaisala.com>
2022-11-16 14:53:21 +0200

    Github ssh key setup

--- macosSetUp.sh
+++ macosSetUp.sh
@@ -51,3 +51,25 @@ echo "Configuring Git"
 git config --global user.name $GITUSER
 git config --global user.email $GITEMAIL
 git config --global core.pager "less -FX"
+
+# Generate SSH keys for github
+read -p 'Personal Github ssh key name: ' GHSSHKEY
+ssh-keygen -t ed25519 -C $GITEMAIL
+
+# Start ssh agent
+eval "$(ssh-agent -s)"
+
+# Configure github ssh to use the generated ssh key
+touch ~/.ssh/config
+echo "
+# Github config
+Host github.com
+  AddKeysToAgent yes
+  UseKeychain yes
+  IdentityFile ~/.ssh/$GHSSHKEY" >> ~/.ssh/config
+
+# Add the ssh key to keychain
+ssh-add --apple-use-keychain ~/.ssh/$GHSSHKEY
+
+echo "Remember to add the $GHSSHKEY to Github ssh keys"
+echo "More info at https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account"
